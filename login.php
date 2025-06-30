35a21c15f2d67a7d409003792f1ad55946f5ed68
Kimi <kimikraus@web.de>
2025-06-30 15:12:05 +0200

    Login

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Rezepte</title>
+        <link rel="stylesheet" href="style.css">
+
+        <!-- /// BOOTSTRAP /// -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- /// ENDE BOOTSTRAP /// -->
+    </head>
+    <body>
+        <div class="login-container">
+        <h2>Login</h2>
+        <form>
+            <label for="email">E-Mail</label>
+            <input type="email" id="email" name="email" placeholder="E-Mail eingeben" required>
+
+            <label for="password">Passwort</label>
+            <input type="password" id="password" name="password" placeholder="Passwort eingeben" required>
+
+            <div class="forgot-password">Passwort vergessen?</div>
+
+            <button type="submit">Anmelden</button>
+        </form>
+        </div>
+    </body>
+</html>
