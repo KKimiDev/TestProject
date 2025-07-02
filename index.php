d69744d9f57cb1cb9d5fdea803fcd7a678dfb565
Kimi <kimikraus@web.de>
2025-07-02 14:11:52 +0200

    head fix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once("check_login.php");
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Rezepte Website</title>
-  <?php include("templates/head.php;"); ?>
+  <?php include("templates/head.php"); ?>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
   <!-- Dein custom CSS -->
   <style>
