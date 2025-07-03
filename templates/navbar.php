2c6101a8345056af29e2f4acc0b1ab6dfb86004c
Kimi <kimikraus@web.de>
2025-07-03 09:16:41 +0200

    fix

--- templates/navbar.php
+++ templates/navbar.php
@@ -19,7 +19,7 @@
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
       <div class="container">
-        <a class="navbar-brand fw-bold text-warning" href="index">RezepteSite</a>
+        <a class="navbar-brand fw-bold text-warning" href="http://localhost/sites/Rezepte/">RezepteSite</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
